91981d546e73fd1b6aea0f80b649dbc3317ebeba
Thom Hunkin <thom.hunkin@gmail.com>
2024-12-08 10:44:30 +0000

    Finished

--- map.js
+++ map.js
@@ -6,14 +6,35 @@
 
 // Using a for loop
 
+let nums = [1, 2, 3, 4, 5, 6];
+let result = [];
+for (let num of nums) {
+  result.push(num * 2);
+}
+
+console.log("First:", result);
 
 // Using map()
+
+const multiByTwo = function (num) {
+  return num * 2;
+}
+
+const mapResult = nums.map(multiByTwo);
+console.log("Second:", mapResult);
 
 
 // Simplified w/ map()
 
+const simMap = nums.map(function (num) { return num * 2; });
+
+console.log("Third:", simMap);
 
 // Simplfied w/ map() + arrow function
+
+const simplified = nums.map(num => num * 2);
+
+console.log("Fourth:", simplified);
 
 
 // With objects:
@@ -37,3 +58,6 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+const id = students.map(student => ({ name: student.name, id: student.id, age: student.id === 1 ? 25 : student.id === 2 ? 30 : 35, }));
+console.log(id);
